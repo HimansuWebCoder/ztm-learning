482e853d81ed5c5497af753b652fe57bc91542ca
HimansuWebCoder <113868318+Hima-123797@users.noreply.github.com>
2023-07-25 10:11:26 +0500

    Andrei All Tips & Secrets Added

---
+++ Andrei Tips  & Secret/AndreiTips.js
@@ -0,0 +1,144 @@
+//? Andrei neagoie
+
+// We're about to take the biggest step in our developer careers now.
+// This is by far my favourite thing to teach.
+// And  I cannot wait to get started.But before I do I want you know one thing:
+// When I first started learning Javascript it was the hardest thing to get.
+
+// I remember feeling confused and frustrated along the way and to be honest if this is your first time,
+// learning a programming language you'll have those moments.
+
+// I'me here to tell you to stick through it. Just like learning any language;
+// with enough time in practice, you can become fluent.
+
+// Now unlike human language though, you don't have to remember as many words, and sentence structure. So you
+// will get there I promise you this.
+
+// Now don't just watch the videos in this section and fly through the 
+// content. Really work on the exercises I give you at the end of the videos and make sure you understand each topic.
+
+
+
+//? The Dirty Little Secret To Become An expert
+//?  - Andrei neagoie
+
+//  Andrei Neagoie
+//  I'm going to tell you a little secret, a secret that not many people tell you about.
+ 
+//  But you may have thought to yourself. Hey, Andrei, how do you know all these things?
+ 
+//  Or anybody that you see online that maybe you watched their YouTube videos or take their courses or even your college professor?
+ 
+//  How do they know all these things?
+ 
+//  It seems so hard to get to where you want to be, right?
+ 
+//  But there's a secret.
+ 
+//  It's not that hard.
+ 
+//  As a matter of fact, anybody that's teaching you online, even myself, teaching, you react right now.
+ 
+//  I was in your shoes.
+ 
+//  I didn't know react before, but now I do.
+ 
+//  Aa a matter of fact, in my past role, I used to get paid as a consultant, as a react consultant in Silicon Valley, a lot of money per hour to advise on react projects for big companies.
+ 
+//  But was I an expert?
+ 
+//  I mean, yes, but how did I become an expert?
+ 
+//  You see, a lot of people think that learning involves finding the right course, finding the right YouTube video, reading the right blog post.
+ 
+//  But there's something that a lot of poeple forget.
+ 
+//  The people that know react the best or any library, any tool for that matter, are the people that create it.
+ 
+//  You're never going to be as good as the people that create that product.
+ 
+//  Like, let's say, react, but you can become almost as good as them.
+ 
+//  And the best way to do that is to read documentation.
+ 
+//  You see, we live in a world where we want the quick, easy fix.
+ 
+//  QUICK, EASY FIX
+ 
+//  I just want to learn, react fast.
+ 
+//  So I'm going to watch a couple of YouTube videos or a couple of tutorials and learn.
+ 
+//  And yes, you can do that.
+ 
+//  But most people as a matter of fact, I very rarely in my profession met people that read the full documentation.
+ 
+//  Do you know how you become the best of the best when using React?
+ 
+//  Well, you go to their documentation website and you literally read everything.
+ 
+//  They have a learn React section, they have references, they have a blog that they do updates on.
+ 
+//  If you read this, you're literally going to be better than most people out there.
+ 
+//  Why?
+ 
+//  Because this is written by the people that have created React.
+ 
+//  Their jog is literally to create documentation to make their library or tool as easy as possible to use.
+ 
+//  So they're going to put the most effort out of anybody to explain how things work, to explain how to do things right.
+ 
+//  And that's the dirty little secret.
+ 
+//  You watch all these people online, including myself, that teach you how to use a tool, but how those people learned how to use that tool is by reading documentation.
+ 
+//  I'VE READ THE DOCUMENTATION
+ 
+//  So you can bypass a lot of this.
+ 
+//  Who should I learn from?
+ 
+//  What videos should I watch?
+ 
+//  What blog post should I read?
+ 
+//  By just reading the documentation.
+ 
+//  That's how I was able to be a consultant at a big company in Silicon Valley.
+ 
+//  I wasn't doing anything exceptional.
+ 
+//  I simply read every single piece of documentation in React until I understood it.
+ 
+//  EVERY SINGLE PIECE 
+ 
+//  From there, you gain experience by building things, by working at a company and seeing how to use react in production.
+ 
+//  PRODUCTION
+ 
+//  But the baseline, the most important thing is to create this foundation of how things work.
+ 
+//  Now, I know this isn't the most exciting video.
+ 
+//  We didn't build anything.
+ 
+//  You just listened to me.
+ 
+//  But this is the best piece of advice that I can give you.
+ 
+//  MOST ONLINE INSTRUCTORS AREN'T ACTUAL EXPERTS IN THE FIELD WITH "EXPERIENCE". THEY ARE SIMPLY TEACHING YOU BASED OFF OF READING DOCUMENTATION.
+ 
+//  You don't necessarily need to always find online courses to teach you something.
+ 
+//  If you want to learn a tool, you're smart enough now to read documentation on your own and try things on your own.
+ 
+//  Don't think that documentation is not sexy enough.
+ 
+//  It's too boring to read.
+ 
+//  You have to do the hard things if you want to be good at something and reading documentation is the best thing you can do.
+ 
+//  So use my videos for React as a reference.
+ 
+//  But after this course, if you really want to dive into React, read the docuementation and that's going to put you above most people because most people unfortunately are too lazy to read what's right in front of them and the best tool to learn something.
